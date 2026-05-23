5220eae9001a92a17cdbd92156a745893ee674ac
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2026-05-23 13:16:43 +0530

    Added SearchContext class to support Cucumber pico container

---
+++ src/test/java/contexts/ScenarioContext.java
@@ -0,0 +1,12 @@
+package contexts;
+
+import pageObjects.AccountsPage;
+import pageObjects.HomePage;
+import pageObjects.RegisterPage;
+
+public class ScenarioContext {
+    public HomePage homePage;
+    public RegisterPage registerPage;
+    public AccountsPage accountPage;
+}
+
